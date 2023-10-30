bd99d818eec8699312ae23daac6a28acea726d80
Victor Lucena <victor.lucenaaa@gmail.com>
2023-10-30 14:29:48 -0300

    feat: Criação dos enuns Via e Labaratorio

---
+++ src/main/java/com/remedios/victor/curso/dto/Laboratorio.java
@@ -0,0 +1,7 @@
+package com.remedios.victor.curso.dto;
+
+public enum Laboratorio {
+
+    MEDLEY,
+    ACHE,
+}
---
+++ src/main/java/com/remedios/victor/curso/dto/Via.java
@@ -0,0 +1,10 @@
+package com.remedios.victor.curso.dto;
+
+public enum Via { // enum são valores constantes que não são alterados
+    //Todas as propriedades dos enuns são em letras maiúsculas e separadas por vírgulas.
+    ORAL,
+    NASAL,
+    VENOSO,
+    INTRAMUSCULAR,
+    RETAL
+}
