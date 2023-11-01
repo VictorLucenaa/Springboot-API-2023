c139ba006fa6baeca27a9f9381acfaf186a198e2
Victor Lucena <victor.lucenaaa@gmail.com>
2023-11-01 15:48:37 -0300

    feat: Criação da classe RemedioController

--- src/main/java/com/remedios/victor/curso/controllers/RemedioController.java
+++ src/main/java/com/remedios/victor/curso/controllers/RemedioController.java
@@ -1,14 +1,19 @@
 package com.remedios.victor.curso.controllers;
 
 import com.remedios.victor.curso.dto.DadosCadastroRemedio;
+import com.remedios.victor.curso.dto.Remedio;
+import com.remedios.victor.curso.repository.RemedioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/remedios") // endpoint da aplicação que este controller é rodado
 public class RemedioController {
 
+    @Autowired
+    private RemedioRepository repository;
     @PostMapping // para salvar dados no banco
-    public void cadastrar(@RequestBody DadosCadastroRemedio dados){ // requestbody faz com que o programa entenda o "corpo" do json e o exiba no console.
-        System.out.println(dados);
+    public void cadastrar(@RequestBody DadosCadastroRemedio dados){
+        repository.save(new Remedio(dados));
     }
 }
